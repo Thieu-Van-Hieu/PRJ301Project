f2d0ed98e0a01e315a21b82e7e9bcfccfc240785
Mr.NoBody <thieuefvanwhieues@gmail.com>
2025-03-09 16:16:10 +0700

    Create base file

--- src/java/controler/FirstServlet.java
+++ src/java/controler/FirstServlet.java
@@ -48,6 +48,7 @@ public class FirstServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         processRequest(request, response);
+        
     }
 
     /** Returns a short description of the servlet. */

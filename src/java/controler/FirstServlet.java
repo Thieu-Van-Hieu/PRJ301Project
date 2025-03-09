96636c8a809e8d09539a2629f231e13343860c0d
Mr.NoBody <thieuefvanwhieues@gmail.com>
2025-03-09 16:18:13 +0700

    Create base folder

--- src/java/controler/FirstServlet.java
+++ src/java/controler/FirstServlet.java
@@ -17,40 +17,39 @@ import jakarta.servlet.http.HttpServletResponse;
  */
 public class FirstServlet extends HttpServlet {
 
-    /** 
+    /**
      * Handles requests for both HTTP GET and POST methods.
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet FirstServlet</title>");  
+            out.println("<title>Servlet FirstServlet</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet FirstServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     /** Handles the HTTP GET method. */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
     /** Handles the HTTP POST method. */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-        
     }
-
+    
     /** Returns a short description of the servlet. */
     @Override
     public String getServletInfo() {

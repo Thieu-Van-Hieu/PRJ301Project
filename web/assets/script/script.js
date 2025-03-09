a58b734d9e983279f58910a36d461e4def94b7c2
Mr.NoBody <thieuefvanwhieues@gmail.com>
2025-03-09 20:54:28 +0700

    Sửa xung đột của thư mục

--- web/assets/script/script.js
+++
@@ -1,8 +0,0 @@
-function move(element) {
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
-    element.style.position = "absolute";
-    element.style.left = x + "px";
-    element.style.top = y + "px";
-    console.log("Moved to " + x + " " + y);
-}

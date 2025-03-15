d707b3f2c2ab7918f987733e64b3efe39cbe1176
Mr.NoBody <thieuefvanwhieues@gmail.com>
2025-03-15 15:23:22 +0700

    Nhập 2 nhánh hauhoa + main

---
+++ web/assets/js/loginbackground.js
@@ -0,0 +1,37 @@
+function updateGreeting() {
+    let now = new Date();
+    let hour = now.getHours();
+    let greetingText = "";
+
+    if (hour >= 5 && hour < 12) {
+        greetingText = "Chào buổi sáng";
+    } else if (hour >= 12 && hour < 18) {
+        greetingText = "Chào buổi trưa";
+    } else {
+        greetingText = "Chào buổi tối";
+    }
+
+    document.getElementById("greeting").innerText = greetingText;
+}
+
+updateGreeting();
+
+function updateBackground() {
+    let now = new Date();
+    let hour = now.getHours();
+    let container = document.querySelector(".container");
+
+    if (hour >= 5 && hour < 12) {
+        container.style.backgroundImage = "url('../assets/image/loginmorning.jpg')";
+    } else if (hour >= 12 && hour < 18 ) {
+        container.style.backgroundImage = "url('../assets/image/loginmorning.jpg')";
+    } else{
+        container.style.backgroundImage = "url('../assets/image/loginnight.jpg')";
+    }
+    container.style.backgroundSize = "cover";
+    container.style.backgroundPosition = "center";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.transition = "background 0.5s ease-in-out";
+}
+
+updateBackground();

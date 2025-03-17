3784c9db52157bdbee9c94a9c41f2b5fa80cc289
Mr.NoBody <thieuefvanwhieues@gmail.com>
2025-03-18 06:38:13 +0700

    Tạo đối tượng Member tạm thời

---
+++ src/java/model/Member.java
@@ -0,0 +1,25 @@
+public class Member {
+    private int id;
+    private String name;
+
+    public Member() {
+    }
+
+    public getId() {
+        return id;
+    }
+
+    public Member setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Member setName(String name) {
+        this.name = name;
+        return this;
+    }
+}

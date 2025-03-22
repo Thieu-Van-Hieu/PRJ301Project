ee7a356d082c19ebb7de637d500153f71b65d587
Mr.NoBody <thieuefvanwhieues@gmail.com>
2025-03-22 18:33:10 +0700

    Xóa bớt phương thức

--- src/java/dto/CalendarResponse.java
+++ src/java/dto/CalendarResponse.java
@@ -7,17 +7,15 @@ public class CalendarResponse {
     private String name;
     private Timestamp startDate;
     private Timestamp endDate;
-    private String status;
 
     public CalendarResponse() {
     }
 
-    public CalendarResponse(int id, String name, Timestamp startDate, Timestamp endDate, String status) {
+    public CalendarResponse(int id, String name, Timestamp startDate, Timestamp endDate) {
         this.id = id;
         this.name = name;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.status = status;
     }
 
     public int getId() {
@@ -51,13 +49,4 @@ public class CalendarResponse {
     public void setEndDate(Timestamp endDate) {
         this.endDate = endDate;
     }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
 }
